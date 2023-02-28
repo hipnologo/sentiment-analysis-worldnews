1bd86343813678daef000797b7f1f001d1d89464
Carvalho, Fabio <Fabio_Carvalho@comcast.com>
2023-02-28 11:35:34 -0500

    Updated README

---
+++ README.md
@@ -0,0 +1,35 @@
+# Sentiment Analysis of World News
+
+This is a Python script that loads news headlines from around the world using the News API, applies sentiment analysis to the headlines, and displays the results in a Streamlit dashboard. The sentiment analysis is performed using the VADER sentiment analysis tool.
+
+## Requirements
+- pandas
+- numpy
+- matplotlib
+- seaborn
+- nltk
+- pickle
+- streamlit
+- requests
+- json
+- plotly.express
+- plotly.graph_objects
+- vaderSentiment
+
+## Usage
+1. Clone the repository:
+```bash
+git clone https://github.com/username/repo.git
+cd repo
+
+2. Install the required packages:
+pip install -r requirements.txt
+
+3. Run the script:
+streamlit run sentiment_analysis.py
+
+4. Wait for the news to load from the News API.
+
+5. Select a news source from the sidebar to filter the headlines and see the sentiment analysis score for that source.
+
+6. The dashboard will display a table of the news headlines and their sentiment scores.
--- app.py
+++ app.py
@@ -11,6 +11,25 @@ import plotly.express as px
 import plotly.graph_objects as go
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+"""
+This script loads news headlines from around the world using the News API, applies sentiment analysis to the headlines,
+and displays the results in a Streamlit dashboard. The sentiment analysis is performed using the VADER sentiment analysis tool.
+
+The dashboard allows the user to select a news source from the list of unique authors in the news headlines dataset, and displays
+the sentiment analysis score for the selected news source, as well as a table of the news headlines and their sentiment scores.
+
+This script requires pandas, numpy, matplotlib, seaborn, nltk, pickle, streamlit, requests, json, plotly.express, plotly.graph_objects,
+and vaderSentiment to be installed.
+
+Usage:
+  - Run the script.
+  - Wait for the news to load from the News API.
+  - Select a news source from the sidebar to filter the headlines and see the sentiment analysis score for that source.
+  - The dashboard will display a table of the news headlines and their sentiment scores.
+
+Author: Fabio Carvalho
+Inspired by: Aditya Verma 
+"""
 
 # Import news from around the world using a public API
 world_news_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=4d1c65d85f4347e4913e2ed023cf0bf2'
@@ -36,8 +55,6 @@ selected_news_author = st.sidebar.selectbox('News Source', ('All', *news_author)
 # update the DataFrame based on the news source selected
 selected_world_news = world_news[world_news['author'] == selected_news_author] if selected_news_author != 'All' else world_news
 
-
-
 # Apply a sentiment analysis to the news headlines
 score = SentimentIntensityAnalyzer().polarity_scores(selected_world_news['title'])
 st.sidebar.write('---')
@@ -50,3 +67,8 @@ st.sidebar.write('Positive' if score['compound'] >= 0.05 else 'Negative' if scor
 
 # Show the news headlines and sentiment scores in a table
 st.table(selected_world_news[['title', 'description', 'author']])
+
+# Save sentiment analysis scores to a pickle file 
+file = open('sentiment_analysis_scores.pkl', 'wb')
+st.write('Saving sentiment analysis scores to a pickle file...')
+st.caption('Done!')
